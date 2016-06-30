a02ff2c440f5be22c62713e36c1b87733691bdb2
Enric Morales <me@enric.me>
2016-06-30 14:51:19 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+# PBNS: Pushbullet Notifications via D-BUS
+
+PBNS is an application that uses the `pushbullet.py` module by @randomchars to
+integrate [Pushbullet](https://www.pushbullet.com) notifications and pushes
+into your desktop. It forwards both mirrored notifications and regular text
+pushes (such as from chat messages) to D-Bus so that they show up via your
+regular notifications daemon.
+
+Click the following screenshot to view a video of PBNS in action.
+
+[![thumb](http://i.imgur.com/EdenPsl.jpg)](https://gfycat.com/ChillyMemorableBuck)
+
+# Features
+
+* Mirrors your devices' notifications to your desktop
+* Displays your pushes as soon as they're received
+* Decryption of your End-to-End-encrypted pushes and notifications
+
+
+# Why
+
+The motivation behind the development of this app was to experiment with the
+Pushbullet API, so I could integrate the notifications with my desktop.
+
+
+# How to run
+
+1. `pip install requirements.txt`
+2. `git clone htts://github.com/kiike/pbns`
+3. source `$YOUR_VIRTUALENV/bin/activate`
+4. `python $CLONE_DIR/pbns.py`
+
+# License and contributions
+
+The Pushbullet.com logo under `resources` directory is property of Pushbullet,
+Inc.
+
+This project is licensed under the ISC license. For details, see the `LICENSE`
+file.
+
+I welcome contributions and suggestions very much. Please file bugs and (pull)
+requests!
---
+++ pbns.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+"""
+A notification handler for Pushbullet that sends pushes via dbus.
+"""
+
+import argparse
+import json
+import logging
+import os
+import sys
+
+import dbus
+import pushbullet
+
+HTTP_PROXY_HOST = None
+HTTP_PROXY_PORT = None
+
+CONFIG_BASEDIR = os.path.join(os.path.expanduser("~"), ".config", "pbns")
+API_KEY_PATH = os.path.join(CONFIG_BASEDIR, "apikey")
+PASSWORD_PATH = os.path.join(CONFIG_BASEDIR, "password")
+
+ICON = os.path.join(os.path.abspath(sys.path[0]), "resources", "pb_logo.png")
+
+
+def get_encryption_password(passwd_path):
+    """
+    Return the encryption password
+    """
+
+    try:
+        with open(passwd_path) as passwd_file:
+            password = passwd_file.read()
+            password = password.strip()
+
+        return password
+
+    except FileNotFoundError:
+        no_crypto_warn = ("Can't use encryption due to inexistent password. "
+                          "If you wish to use encryption, place your "
+                          "encryption password into %s")
+        logging.warning(no_crypto_warn, passwd_path)
+
+
+def get_api_key(api_key_path):
+    """
+    Return the API key from config file
+    """
+
+    try:
+        with open(api_key_path) as api_key:
+            api_key = api_key.read()
+            api_key = api_key.strip()
+
+        return api_key
+
+    except FileNotFoundError:
+        err_msg = ("Couldn't load your access token. Create one at "
+                   "'https://www.pushbullet.com/#settings/account' "
+                   "and paste it into '{}'.")
+
+        err_msg = err_msg.format(API_KEY_PATH)
+        logging.error(err_msg)
+        sys.exit(1)
+
+
+def notify(title, body):
+    logging.debug("Sending notification via D-Bus")
+
+    interface = "org.freedesktop.Notifications"
+    item = "org.freedesktop.Notifications"
+    path = "/org/freedesktop/Notifications"
+
+    bus = dbus.SessionBus()
+    proxy = bus.get_object(item, path)
+    notifications = dbus.Interface(proxy, interface)
+
+    title = title.strip()
+    body = body.strip()
+
+    notifications.Notify("Pushbullet", 0, ICON, title, body, "", "", 5000)
+
+
+def handle_mirror(push):
+    """
+    Returns two strings:
+        1. title in the format '[appname] push title'
+        2. push body
+    """
+
+    title = "[{}] {}"
+    title = title.format(push["application_name"], push["title"])
+
+    return title, push["body"]
+
+
+def handle_push(push):
+    """
+    Returns two strings: the title and the body
+    """
+
+    return push["title"], push["body"]
+
+
+def check_if_dismissed(push):
+    """
+    Returns True if `push` is dismissed
+    """
+
+    return "dismissed" in push.keys() and push["dismissed"]
+
+
+def on_push(push):
+    """
+    Handle last push
+    """
+
+    title, body = None, None
+
+    logging.debug("Got push: %s", push)
+
+    if push["type"] == "tickle":
+        # Overwrite push with latest push
+        push = PB.get_pushes()[0]
+
+        logging.debug("Last push: %s", push)
+        title, body = handle_push(push)
+
+    elif push["type"] == "push":
+        push = push["push"]
+
+        logging.debug("Push contents: %s", push)
+        if push["encrypted"]:
+            decrypted = PB._decrypt_data(push["ciphertext"])
+            push = json.loads(decrypted)
+            logging.debug("Decrypted contents: %s", push)
+
+        if push["type"] == "mirror":
+            title, body = handle_mirror(push)
+
+    if title and body and not check_if_dismissed(push):
+        notify(title, body)
+
+
+def main():
+    """
+    Initialize app and listen for new pushes
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="Output debugging information",
+                        action="store_true")
+    args = parser.parse_args()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    logging.basicConfig(format="%(asctime)s [%(levelname)5s] %(message)s")
+
+    api_key = get_api_key(API_KEY_PATH)
+    password = get_encryption_password(PASSWORD_PATH)
+
+    global PB
+    PB = pushbullet.Pushbullet(api_key, encryption_password=password)
+
+    listener = pushbullet.Listener(account=PB,
+                                   on_push=on_push,
+                                   http_proxy_host=HTTP_PROXY_HOST,
+                                   http_proxy_port=HTTP_PROXY_PORT)
+    try:
+        listener.run_forever()
+    except KeyboardInterrupt:
+        listener.close()
+
+
+if __name__ == "__main__":
+    main()
